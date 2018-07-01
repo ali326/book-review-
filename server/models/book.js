101e16ca280a063b3da545cb0ddd92a113aa45ed
diego7963 <elshrab90.ma@gmail.com>
2018-07-01 19:44:40 +0200

    user added

---
+++ server/models/book.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const bookSchema = mongoose .Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    review: {
+        type: String,
+        default: 'n/a'
+    },
+    pages: {
+        type: String,
+        default: 'n/a'
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5
+    },
+    price: {
+        type: String,
+        defautl: 'n/a'
+    },
+    ownerId: {
+        type: String,
+        required: true
+    }
+},{timestamps: true})
+
+const Book = mongoose.model('Book',bookSchema);
+module.exports = {Book}
